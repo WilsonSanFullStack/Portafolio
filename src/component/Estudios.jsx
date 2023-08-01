dee811bea773eb1fe2ee2977d8b8464ecff56be1
ryuksan <clinicasystemlab@gmail.com>
2023-07-31 23:07:42 -0500

    cambios

--- src/component/Estudios.jsx
+++ src/component/Estudios.jsx
@@ -5,73 +5,73 @@ const Estudios = () => {
   return (
     <div className={styles.container}>
       <div className={`${styles.basico} ${styles.column}`}>
-        <h1 id={styles.h1}>ESTUDIOS Basicos</h1>
-        <h2 id={styles.h2}>Primaria</h2>
-        <p id={styles.p}>
+        <h1 className={styles.h1}>ESTUDIOS Basicos</h1>
+        <h2 className={styles.h2}>Primaria</h2>
+        <p className={styles.p}>
           Educacion primaria cursada en la Escuela Primaria Mundo Nuevo
           perteneciente al Colegio Nacionalizado Pablo VI del municipio de
           Cubara Boyaca.
         </p>
-          <p id={styles.p}>
-            <strong id={styles.strong}>Del 2006 al 2009</strong>
+          <p className={styles.p}>
+            <strong className={styles.strong}>Del 2006 al 2009</strong>
           </p>
-        <h2 id={styles.h2}>Secundaria</h2>
-        <p id={styles.p}>
+        <h2 className={styles.h2}>Secundaria</h2>
+        <p className={styles.p}>
           Estudios realizados en el Colegio Agricola Gibaltar del Municipio de
           Toledo Norte de Santander.
         </p>
-          <p id={styles.p}>
-            <strong id={styles.strong}>Del 2010 al 2013</strong>
+          <p className={styles.p}>
+            <strong className={styles.strong}>Del 2010 al 2013</strong>
           </p>
-          <h2 id={styles.h2}>Graduacion</h2>
-          <p id={styles.p}>
+          <h2 className={styles.h2}>Graduacion</h2>
+          <p className={styles.p}>
             Examen Nacional con el Instituto Colombiano para la Evaluación de la
-            Educación <strong id={styles.strong}>ICFES</strong>
+            Educación <strong className={styles.strong}>ICFES</strong>
           </p>
-            <p id={styles.p}>
-              <strong id={styles.strong}>
+            <p className={styles.p}>
+              <strong className={styles.strong}>
                 Abril Del 2014 Bucaramanga Santander
               </strong>
             </p>
       </div>
       <div className={`${styles.uni} ${styles.column}`}>
-        <h1 id={styles.h1}>Estudios De Carrera</h1>
-        <h2 id={styles.h2}>Carrera Tecnica De Topografia</h2>
-        <p id={styles.p}>
+        <h1 className={styles.h1}>Estudios De Carrera</h1>
+        <h2 className={styles.h2}>Carrera Tecnica De Topografia</h2>
+        <p className={styles.p}>
           Estudios de Topografia realizados en las
-          <strong id={styles.strong}>Unidades Tecnologicas de Santander</strong>
+          <strong className={styles.strong}>Unidades Tecnologicas de Santander</strong>
           donde estuve por 4 semestres los cuales pase con buenas notas dado la
           situacion economica del momento no termine los estudios.
         </p>
-          <p id={styles.p}>
-            <strong id={styles.strong}>Del 2015 al 2017</strong>
+          <p className={styles.p}>
+            <strong className={styles.strong}>Del 2015 al 2017</strong>
           </p>
-        <h2 id={styles.h2}>
+        <h2 className={styles.h2}>
           Curso en Ensamble y Reparacion De Equipos De Computo
         </h2>
-        <p id={styles.p}>
+        <p className={styles.p}>
           Curso intensivo de 60 horas Realizado en Corporacion Educativa De
           Santander con lo cual estuve ejerciendo como tecnico en una empresa
           durante 3 años.
         </p>
-          <p id={styles.p}></p>
-          <strong id={styles.strong}>Diciembre Del 2018</strong>
-        <h2 id={styles.h2}>Boot Camp HENRY Full Stack Developer</h2>
-        <p id={styles.p}>
+          <p className={styles.p}></p>
+          <strong className={styles.strong}>Diciembre Del 2018</strong>
+        <h2 className={styles.h2}>Boot Camp HENRY Full Stack Developer</h2>
+        <p className={styles.p}>
           Estudios realizados virtualmente durante 4 meses en donde aprendi el
-          manejo de <strong id={styles.strong}>JavaScript</strong> y Frameworks
+          manejo de <strong className={styles.strong}>JavaScript</strong> y Frameworks
           como:
-          <strong id={styles.strong}> React.js</strong>,{" "}
-          <strong id={styles.strong}> Node.js</strong>,
-          <strong id={styles.strong}> Express.js</strong>,{" "}
-          <strong id={styles.strong}> Redux.js</strong>,{" "}
-          <strong id={styles.strong}> Sequelize</strong>,{" "}
-          <strong id={styles.strong}> PostgreSQL</strong>,{" "}
-          <strong id={styles.strong}> HTML</strong>,{" "}
-          <strong id={styles.strong}> CSS</strong>.
+          <strong className={styles.strong}> React.js</strong>,{" "}
+          <strong className={styles.strong}> Node.js</strong>,
+          <strong className={styles.strong}> Express.js</strong>,{" "}
+          <strong className={styles.strong}> Redux.js</strong>,{" "}
+          <strong className={styles.strong}> Sequelize</strong>,{" "}
+          <strong className={styles.strong}> PostgreSQL</strong>,{" "}
+          <strong className={styles.strong}> HTML</strong>,{" "}
+          <strong className={styles.strong}> CSS</strong>.
         </p>
-          <p id={styles.p}>
-            <strong id={styles.strong}>
+          <p className={styles.p}>
+            <strong className={styles.strong}>
               Del 10 Abril del 2023 al 1 Agosto Del 2023
             </strong>
           </p>
--- src/component/Experiencia.jsx
+++ src/component/Experiencia.jsx
@@ -4,13 +4,13 @@ import styles from '../styles/Experiencia.module.css'
 const Experiencia = () => {
   return (
     <div className={styles.container}>
-      <h1 id={styles.h1}>EXPERIENCIA</h1>
+      <h1 className={styles.h1}>EXPERIENCIA</h1>
       <div>
-          <h2 id={styles.h2}>Full Stack Developer Junior</h2>
-          <h3 id={styles.h3}>Pagina De Encuentros</h3>
-          <h4 id={styles.h4}>Nomand Locals,</h4>
-          <h4 id={styles.h4}>Colombia.</h4>
-          <p id={styles.p}>
+          <h2 className={styles.h2}>Full Stack Developer Junior</h2>
+          <h3 className={styles.h3}>Pagina De Encuentros</h3>
+          <h4 className={styles.h4}>Nomand Locals,</h4>
+          <h4 className={styles.h4}>Colombia.</h4>
+          <p className={styles.p}>
             Diseñar y desarrollar Pagina Web de encuentros mediante eventos para
             hacer posible el encutro entre dos o varias personas en un mismo
             punto para la socializacion. Funcionalidades de la pagina:
@@ -42,14 +42,14 @@ const Experiencia = () => {
             <li>Desarrollar sistema de chat general y personal. </li>
             <li>Desarrollar sistema de inicio de sesión con terceros</li>
           </ul>
-        <p id={styles.p}>
-          <strong id={styles.strong}>Del 27 de Junio Al 31 de Agosto 2023</strong>
+        <p className={styles.p}>
+          <strong className={styles.strong}>Del 27 de Junio Al 31 de Agosto 2023</strong>
         </p>
-        <h4 id={styles.h4}>Proyecto Desarrollado Con Colaboracion de:</h4>
+        <h4 className={styles.h4}>Proyecto Desarrollado Con Colaboracion de:</h4>
         <div className={styles.section}>
           <section>
-            <h5 id={styles.h5}>Wilson Sanchez</h5>
-            <h5 id={styles.h5}>Back-End</h5>
+            <h5 className={styles.h5}>Wilson Sanchez</h5>
+            <h5 className={styles.h5}>Back-End</h5>
             <ul>
               <li>
                 <a href="https://github.com/WilsonSanFullStack">GitHub</a>
@@ -61,8 +61,8 @@ const Experiencia = () => {
           </section>
 
           <section>
-            <h5 id={styles.h5}>Juan Tosso</h5>
-            <h5 id={styles.h5}>Front-End</h5>
+            <h5 className={styles.h5}>Juan Tosso</h5>
+            <h5 className={styles.h5}>Front-End</h5>
             <ul>
               <li>
                 <a href="https://github.com/JuanTosso">GitHub</a>
@@ -76,8 +76,8 @@ const Experiencia = () => {
           </section>
 
           <section>
-            <h5 id={styles.h5}>Pablo Bottoni</h5>
-            <h5 id={styles.h5}>Front-End</h5>
+            <h5 className={styles.h5}>Pablo Bottoni</h5>
+            <h5 className={styles.h5}>Front-End</h5>
             <ul>
               <li>
                 <a href="https://github.com/PabNic">GitHub</a>
@@ -91,8 +91,8 @@ const Experiencia = () => {
           </section>
 
           <section>
-            <h5 id={styles.h5}>Daniel Gomez</h5>
-            <h5 id={styles.h5}>Back-End</h5>
+            <h5 className={styles.h5}>Daniel Gomez</h5>
+            <h5 className={styles.h5}>Back-End</h5>
             <ul>
               <li>
                 <a href="https://github.com/Danogomez">GitHub</a>
@@ -104,8 +104,8 @@ const Experiencia = () => {
           </section>
 
           <section>
-            <h5 id={styles.h5}>Sebastian Rastelli</h5>
-            <h5 id={styles.h5}>Front-End</h5>
+            <h5 className={styles.h5}>Sebastian Rastelli</h5>
+            <h5 className={styles.h5}>Front-End</h5>
             <ul>
               <li>
                 <a href="https://github.com/sebarastelli?tab=repositories">
@@ -121,8 +121,8 @@ const Experiencia = () => {
           </section>
 
           <section>
-            <h5 id={styles.h5}>David Florido</h5>
-            <h5 id={styles.h5}>Front-End</h5>
+            <h5 className={styles.h5}>David Florido</h5>
+            <h5 className={styles.h5}>Front-End</h5>
             <ul>
               <li>
                 <a href="https://github.com/DavidPaimon">GitHub</a>
@@ -136,10 +136,10 @@ const Experiencia = () => {
       </div>
 
       <div>
-        <h2 id={styles.h2}>Full Stack Developer</h2>
-        <h3 id={styles.h3}>Proyecto Individual</h3>
-        <h5 id={styles.h5}>HENRY</h5>
-        <p id={styles.p}>
+        <h2 className={styles.h2}>Full Stack Developer</h2>
+        <h3 className={styles.h3}>Proyecto Individual</h3>
+        <h5 className={styles.h5}>HENRY</h5>
+        <p className={styles.p}>
           Diseñar y desarrollar una App de países que incluía: autenticación,
           búsquedas, filtrados combinados, ordenamientos, etc. Creador:
           desarrolle el proyecto completamente solo donde aplique los
@@ -148,42 +148,42 @@ const Experiencia = () => {
           funcionalidades y rectificaba su funcionamiento haciendo también la
           mejora de las mismas.
         </p>
-        <p id={styles.p}>
-          <strong id={styles.strong}>Control de versionado:</strong> utilizando GIT fui utilizando
+        <p className={styles.p}>
+          <strong className={styles.strong}>Control de versionado:</strong> utilizando GIT fui utilizando
           diferentes versiones de mi código para llegar a la funcionalidad
           perfecta de la app esto hizo que rápidamente obtuviera un resultado
           productivo y eficaz.
         </p>
-        <p id={styles.p}>
-          <strong id={styles.strong}>Front:</strong> JavaScript, react.js, redux.js.
+        <p className={styles.p}>
+          <strong className={styles.strong}>Front:</strong> JavaScript, react.js, redux.js.
         </p>
-        <p id={styles.p}>
-          <strong id={styles.strong}>Back:</strong> Node.js, express, Axios. Base de datos:
+        <p className={styles.p}>
+          <strong className={styles.strong}>Back:</strong> Node.js, express, Axios. Base de datos:
           PostgreSQL y Sequelize.
         </p>
-        <p id={styles.p}>
-          <strong id={styles.strong}>Del 1 junio del 2023 al 25 junio del 2023</strong>
+        <p className={styles.p}>
+          <strong className={styles.strong}>Del 1 junio del 2023 al 25 junio del 2023</strong>
         </p>
       </div>
       <div>
-        <h2 id={styles.h2}>Full Stack Developer</h2>
-        <h3 id={styles.h3}>Proyecto Integrador</h3>
-        <h5 id={styles.h5}>HENRY</h5>
-        <p id={styles.p}>
+        <h2 className={styles.h2}>Full Stack Developer</h2>
+        <h3 className={styles.h3}>Proyecto Integrador</h3>
+        <h5 className={styles.h5}>HENRY</h5>
+        <p className={styles.p}>
           Diseñar y desarrollar una App de Ryck And Morty que incluía:
           autenticación, búsquedas, filtrados combinados, ordenamientos, etc.
         </p>
-        <p id={styles.p}>
-          <strong id={styles.strong}>Front:</strong> JavaScript, react.js y redux.js.
+        <p className={styles.p}>
+          <strong className={styles.strong}>Front:</strong> JavaScript, react.js y redux.js.
         </p>
-        <p id={styles.p}>
-          <strong id={styles.strong}>Back:</strong> Node.js y Express.js
+        <p className={styles.p}>
+          <strong className={styles.strong}>Back:</strong> Node.js y Express.js
         </p>
-        <p id={styles.p}>
-          <strong id={styles.strong}>Base de datos:</strong> PostgreSQL y Sequelize.
+        <p className={styles.p}>
+          <strong className={styles.strong}>Base de datos:</strong> PostgreSQL y Sequelize.
         </p>
-        <p id={styles.p}>
-          <strong id={styles.strong}>Practica Durante El BootCamp</strong>
+        <p className={styles.p}>
+          <strong className={styles.strong}>Practica Durante El BootCamp</strong>
         </p>
       </div>
     </div>
