e3c14eb5d24185d5e3f05d7c9014a96c7c21f43d
ryuksan <clinicasystemlab@gmail.com>
2023-08-02 11:40:16 -0500

    fuente

--- src/component/Experiencia.jsx
+++ src/component/Experiencia.jsx
@@ -1,51 +1,61 @@
 import React from "react";
-import styles from '../styles/Experiencia.module.css'
+import styles from "../styles/Experiencia.module.css";
 
 const Experiencia = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.h1}>EXPERIENCIA</h1>
       <div>
-          <h2 className={styles.h2}>Full Stack Developer Junior</h2>
-          <h3 className={styles.h3}>Pagina De Encuentros</h3>
-          <h4 className={styles.h4}>Nomand Locals,</h4>
-          <h4 className={styles.h4}>Colombia.</h4>
-          <p className={styles.p}>
-            Diseñar y desarrollar Pagina Web de encuentros mediante eventos para
-            hacer posible el encutro entre dos o varias personas en un mismo
-            punto para la socializacion. Funcionalidades de la pagina:
-          </p>
-          <ul>
-            <li>
-              Desarrollar una página web para diferentes navegadores y
-              dispositivos como móviles y desktop con manejo de diferentes
-              resoluciones “responsivo” usando tecnologías como: JavaScript,
-              React.js, Redux, HTML,CSS, Clerk-React, Axios, Redux-persist,
-              Socket.io, Star-rating, Tailwind-css, Vite, Node.js, Express,
-              PostgreSQL, Sequelize.
-            </li>
-            <li>
-              Diseñar interfaces y elementos Front End, mantener y optimizar
-              sitios web e interfaces.
-            </li>
-            <li>
-              Desarrollar diferentes tipos de usuarios (Administrador, Standar).
-            </li>
-            <li>
-              Desarrollar un motor de búsqueda con filtros como: actividades del
-              usuario, generales, tipo, presupuesto, locación, cercanía,
-              cantidad de personas, duración, fecha.
-            </li>
-            <li>Desarrollar sistema de reportes de usuarios o actividades. </li>
-            <li>Desarrollar sistema de review de usuarios o actividades. </li>
-            <li>Desarrollar sistema de valoración de actividades. </li>
-            <li>Desarrollar sistema de chat general y personal. </li>
-            <li>Desarrollar sistema de inicio de sesión con terceros</li>
-          </ul>
+        <h2 className={styles.h2}>Full Stack Developer Junior</h2>
+        <h3 className={styles.h3}>Pagina De Encuentros</h3>
+        <a href="https://nomadlocals.netlify.app/activities">
+          <h5 className={styles.aa}>Produccion</h5>
+        </a>
+        <a href="https://github.com/WilsonSanFullStack/AppDeEncuentos">
+          <h5 className={styles.aa}>Codigo</h5>
+        </a>
+        <h4 className={styles.h4}>Nomand Locals,</h4>
+        <h4 className={styles.h4}>Colombia.</h4>
         <p className={styles.p}>
-          <strong className={styles.strong}>Del 27 de Junio Al 31 de Agosto 2023</strong>
+          Diseñar y desarrollar Pagina Web de encuentros mediante eventos para
+          hacer posible el encutro entre dos o varias personas en un mismo punto
+          para la socializacion. Funcionalidades de la pagina:
         </p>
-        <h4 className={styles.h4}>Proyecto Desarrollado Con Colaboracion de:</h4>
+        <ul>
+          <li>
+            Desarrollar una página web para diferentes navegadores y
+            dispositivos como móviles y desktop con manejo de diferentes
+            resoluciones “responsivo” usando tecnologías como: JavaScript,
+            React.js, Redux, HTML,CSS, Clerk-React, Axios, Redux-persist,
+            Socket.io, Star-rating, Tailwind-css, Vite, Node.js, Express,
+            PostgreSQL, Sequelize.
+          </li>
+          <li>
+            Diseñar interfaces y elementos Front End, mantener y optimizar
+            sitios web e interfaces.
+          </li>
+          <li>
+            Desarrollar diferentes tipos de usuarios (Administrador, Standar).
+          </li>
+          <li>
+            Desarrollar un motor de búsqueda con filtros como: actividades del
+            usuario, generales, tipo, presupuesto, locación, cercanía, cantidad
+            de personas, duración, fecha.
+          </li>
+          <li>Desarrollar sistema de reportes de usuarios o actividades. </li>
+          <li>Desarrollar sistema de review de usuarios o actividades. </li>
+          <li>Desarrollar sistema de valoración de actividades. </li>
+          <li>Desarrollar sistema de chat general y personal. </li>
+          <li>Desarrollar sistema de inicio de sesión con terceros</li>
+        </ul>
+        <p className={styles.p}>
+          <strong className={styles.strong}>
+            Del 27 de Junio Al 31 de Agosto 2023
+          </strong>
+        </p>
+        <h4 className={styles.h4}>
+          Proyecto Desarrollado Con Colaboracion de:
+        </h4>
         <div className={styles.section}>
           <section>
             <h5 className={styles.h5}>Wilson Sanchez</h5>
@@ -98,7 +108,9 @@ const Experiencia = () => {
                 <a href="https://github.com/Danogomez">GitHub</a>
               </li>
               <li>
-                <a href="https://www.linkedin.com/in/daniel-gomez-fullstack">Linkedin</a>
+                <a href="https://www.linkedin.com/in/daniel-gomez-fullstack">
+                  Linkedin
+                </a>
               </li>
             </ul>
           </section>
@@ -128,7 +140,9 @@ const Experiencia = () => {
                 <a href="https://github.com/DavidPaimon">GitHub</a>
               </li>
               <li>
-                <a href="https://www.linkedin.com/in/david-felipe-florido-jim%C3%A9nez-a862bb262/">Linkedin</a>
+                <a href="https://www.linkedin.com/in/david-felipe-florido-jim%C3%A9nez-a862bb262/">
+                  Linkedin
+                </a>
               </li>
             </ul>
           </section>
@@ -138,6 +152,9 @@ const Experiencia = () => {
       <div>
         <h2 className={styles.h2}>Full Stack Developer</h2>
         <h3 className={styles.h3}>Proyecto Individual</h3>
+        <a href="https://github.com/WilsonSanFullStack/PiCountries">
+          <h5 className={styles.aa}>Codigo</h5>
+        </a>
         <h5 className={styles.h5}>HENRY</h5>
         <p className={styles.p}>
           Diseñar y desarrollar una App de países que incluía: autenticación,
@@ -149,41 +166,51 @@ const Experiencia = () => {
           mejora de las mismas.
         </p>
         <p className={styles.p}>
-          <strong className={styles.strong}>Control de versionado:</strong> utilizando GIT fui utilizando
-          diferentes versiones de mi código para llegar a la funcionalidad
-          perfecta de la app esto hizo que rápidamente obtuviera un resultado
-          productivo y eficaz.
+          <strong className={styles.strong}>Control de versionado:</strong>{" "}
+          utilizando GIT fui utilizando diferentes versiones de mi código para
+          llegar a la funcionalidad perfecta de la app esto hizo que rápidamente
+          obtuviera un resultado productivo y eficaz.
         </p>
         <p className={styles.p}>
-          <strong className={styles.strong}>Front:</strong> JavaScript, react.js, redux.js.
+          <strong className={styles.strong}>Front:</strong> JavaScript,
+          react.js, redux.js.
         </p>
         <p className={styles.p}>
-          <strong className={styles.strong}>Back:</strong> Node.js, express, Axios. Base de datos:
-          PostgreSQL y Sequelize.
+          <strong className={styles.strong}>Back:</strong> Node.js, express,
+          Axios. Base de datos: PostgreSQL y Sequelize.
         </p>
         <p className={styles.p}>
-          <strong className={styles.strong}>Del 1 junio del 2023 al 25 junio del 2023</strong>
+          <strong className={styles.strong}>
+            Del 1 junio del 2023 al 25 junio del 2023
+          </strong>
         </p>
       </div>
       <div>
         <h2 className={styles.h2}>Full Stack Developer</h2>
         <h3 className={styles.h3}>Proyecto Integrador</h3>
+        <a href="https://github.com/WilsonSanFullStack/ft-38a/tree/main/ryckandmorty">
+          <h5 className={styles.aa}>Codigo</h5>
+        </a>
         <h5 className={styles.h5}>HENRY</h5>
         <p className={styles.p}>
           Diseñar y desarrollar una App de Ryck And Morty que incluía:
           autenticación, búsquedas, filtrados combinados, ordenamientos, etc.
         </p>
         <p className={styles.p}>
-          <strong className={styles.strong}>Front:</strong> JavaScript, react.js y redux.js.
+          <strong className={styles.strong}>Front:</strong> JavaScript, react.js
+          y redux.js.
         </p>
         <p className={styles.p}>
           <strong className={styles.strong}>Back:</strong> Node.js y Express.js
         </p>
         <p className={styles.p}>
-          <strong className={styles.strong}>Base de datos:</strong> PostgreSQL y Sequelize.
+          <strong className={styles.strong}>Base de datos:</strong> PostgreSQL y
+          Sequelize.
         </p>
         <p className={styles.p}>
-          <strong className={styles.strong}>Practica Durante El BootCamp</strong>
+          <strong className={styles.strong}>
+            Practica Durante El BootCamp
+          </strong>
         </p>
       </div>
     </div>
